6f8403d0b47e7e337488b2294eb4d55388bc0c4e
hheeseung <ssigu69@gmail.com>
2022-10-30 23:36:05 +0900

    refactor: 필요 없는 라이브러리 import 삭제

--- pages/diary/[postId]/index.tsx
+++ pages/diary/[postId]/index.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery, useMutation, ApolloQueryResult } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
