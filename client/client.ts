dd44abd8b8c7f043807d684b152d0b45a31714e0
hheeseung <ssigu69@gmail.com>
2022-11-01 17:17:27 +0900

    fix: API 주소 설정

--- client/client.ts
+++ client/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://practice.codebootcamp.co.kr/graphql",
+  uri: process.env.NEXT_PUBLIC_API_KEY,
   cache: new InMemoryCache(),
 });
 
