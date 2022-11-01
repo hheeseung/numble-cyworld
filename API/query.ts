b149fe3afc86eb888dc30a9e4b959e6da9a83702
hheeseung <ssigu69@gmail.com>
2022-11-01 23:08:51 +0900

    feat: 쿼리문 파일 생성

---
+++ API/query.ts
@@ -0,0 +1,62 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_POST = gql`
+  mutation createPost($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export const GET_LIST = gql`
+  query getBoards {
+    fetchBoards {
+      number
+      title
+      createdAt
+    }
+  }
+`;
+
+export const GET_POST = gql`
+  query getBoard($postId: Int) {
+    fetchBoard(number: $postId) {
+      number
+      createdAt
+      title
+      contents
+    }
+  }
+`;
+
+export const UPDATE_POST = gql`
+  mutation updatePost(
+    $number: Int
+    $writer: String
+    $title: String
+    $contents: String
+  ) {
+    updateBoard(
+      number: $number
+      writer: $writer
+      title: $title
+      contents: $contents
+    ) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export const DELETE_POST = gql`
+  mutation deletePost($postId: Int) {
+    deleteBoard(number: $postId) {
+      _id
+      number
+      message
+    }
+  }
+`;
