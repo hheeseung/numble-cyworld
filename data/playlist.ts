8184aa70f7aa518596a73f80218d187b44b47b07
hheeseung <ssigu69@gmail.com>
2022-11-01 23:09:08 +0900

    feat: 음악 데이터셋 생성

---
+++ data/playlist.ts
@@ -0,0 +1,44 @@
+import { IPlaylist } from "../types/type";
+
+export const playlist: IPlaylist[] = [
+  {
+    title: "Immigrant Song",
+    artist: "Led Zeppelin",
+  },
+  {
+    title: "Nothin' But A Good Time",
+    artist: "Poison",
+  },
+  {
+    title: "Smell's Like Teen Spirit",
+    artist: "Nirvana",
+  },
+  {
+    title: "Hammer To Fall",
+    artist: "Queen",
+  },
+  {
+    title: "Do I Wanna Know?",
+    artist: "Arctic Monkeys",
+  },
+  {
+    title: "Morning Glory",
+    artist: "Oasis",
+  },
+  {
+    title: "Thunder",
+    artist: "Imagine Dragons",
+  },
+  {
+    title: "Shot In The Dark",
+    artist: "AC/DC",
+  },
+  {
+    title: "Happiness",
+    artist: "The 1975",
+  },
+  {
+    title: "Black Magic",
+    artist: "The Amazons",
+  },
+];
